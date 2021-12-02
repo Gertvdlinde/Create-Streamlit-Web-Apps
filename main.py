d956b2c9cd358703eed8d3a2740c02a49cd8cd16
gertvdlinde <gertvdlinde@gmail.com>
2021-12-02 10:17:22 +0000

    Added Line Chart

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import streamlit as st
+import pandas
 
+data = { 
+  'Series_1':[1,3,4,5,7],
+  'Series_2':[10,30,40,100,250]
+}
+
+df = pandas.DataFrame(data)
 st.title('Our first streamlit app')
 st.subheader (' Introduction to Streamlit')
 st.write('''This is our first web app''')
+st.write(df)
+st.line_chart(df)
